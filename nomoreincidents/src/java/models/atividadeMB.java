571e6601ace8c5bcce6fed851a783a9e1119d667
DiegoKremer <diego.kremer@gmail.com>
2016-03-25 12:51:32 -0300

    Criados demais Models do projeto

---
+++ nomoreincidents/src/java/models/atividadeMB.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package models;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ApplicationScoped;
+
+/**
+ *
+ * @author Diego Kremer
+ */
+@ManagedBean
+@ApplicationScoped
+public class atividadeMB {
+
+    /**
+     * Creates a new instance of atividadeMB
+     */
+    public atividadeMB() {
+    }
+    
+}
