c330cc4ecac7eeff2e4bf43599fd9fd4380a580c
DiegoKremer <diego.kremer@gmail.com>
2016-03-26 23:45:32 -0300

    Criado funcionalidades de Grupo Tecnico

--- nomoreincidents/src/java/models/grupotecnicoMB.java
+++ nomoreincidents/src/java/models/grupotecnicoMB.java
@@ -8,19 +8,68 @@ package models;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ApplicationScoped;
+import javax.inject.Named;
+
 
 /**
  *
  * @author Diego Kremer
  */
+@Named(value = "gtecnicoMB")
 @ManagedBean
 @ApplicationScoped
 public class grupotecnicoMB {
+    
+    private String nome;
+    private String email;
+    private String telefone;
+    private usuarioMB[] membros;
+    
 
     /**
      * Creates a new instance of grupotecnicoMB
      */
     public grupotecnicoMB() {
+        
+        
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public usuarioMB[] getMembros() {
+        return membros;
+    }
+
+    public void setMembros(usuarioMB[] membros) {
+        this.membros = membros;
+    }
+    
+    
+    public void cadastraGrupo() {
+    
+    }
+    
     
 }
