91ee9a029ead8a9bac736396d4a7741c242ca69a
DiegoKremer <diego.kremer@gmail.com>
2016-05-23 19:28:11 -0300

    Atualizacoes UsuarioMB

--- nomoreincidents-bd/src/java/br/com/senacrs/nomoreincidents/mb/UsuarioMB.java
+++ nomoreincidents-bd/src/java/br/com/senacrs/nomoreincidents/mb/UsuarioMB.java
@@ -6,6 +6,7 @@
 
 package br.com.senacrs.nomoreincidents.mb;
 
+import br.com.senacrs.nomoreincidents.model.Usuario;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
@@ -16,11 +17,44 @@ import javax.faces.bean.SessionScoped;
 @ManagedBean
 @SessionScoped
 public class UsuarioMB {
+    
+    /* 
+    Variáveis temporárias de usuário e senha para usar na verificação 
+    dos valores digitados na tela de login com o que está cadastrado no 
+    banco.
+    */ 
+    private String username;
+    private String password;
 
+    // Variável para manipulação do usuário.
+    private Usuario usuarioSelecionado;
+    
+    // Variável para verificação se usuário está logado.
+    private boolean logado;
+    
     /**
      * Creates a new instance of UsuarioMB
      */
     public UsuarioMB() {
+        usuarioSelecionado = new Usuario ();
+    }
+    
+    public String verificaDadosUsuario () {
+        
+     return ("login?faces-redirect=true");
+     
+    }
+    
+    public boolean isAdmin () {
+        return false;
+    }
+    
+    public boolean isUsuario () {
+        return false;
+    }
+    
+    public boolean isLogado () {
+        return logado;
     }
     
 }
