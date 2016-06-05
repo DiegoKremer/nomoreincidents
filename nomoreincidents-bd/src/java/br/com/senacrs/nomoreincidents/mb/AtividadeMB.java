fc7f2fa906eaec632093ac13a6692ecdbc795076
DiegoKremer <diego.kremer@gmail.com>
2016-06-05 20:58:33 -0300

    Criado metodos padroes de AtividadeMB

--- nomoreincidents-bd/src/java/br/com/senacrs/nomoreincidents/mb/AtividadeMB.java
+++ nomoreincidents-bd/src/java/br/com/senacrs/nomoreincidents/mb/AtividadeMB.java
@@ -6,6 +6,10 @@
 
 package br.com.senacrs.nomoreincidents.mb;
 
+import br.com.senacrs.nomoreincidents.business.AtividadeService;
+import br.com.senacrs.nomoreincidents.business.BusinessException;
+import br.com.senacrs.nomoreincidents.model.Atividade;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
@@ -17,10 +21,64 @@ import javax.faces.bean.SessionScoped;
 @SessionScoped
 public class AtividadeMB {
 
+    private Atividade atividadeSelecionada;
+
     /**
-     * Creates a new instance of AtividadeMB
+     * Creates a new instance of GrupoTecnicoMB
      */
     public AtividadeMB() {
+        
+    }
+
+    public Atividade getAtividadeSelecionada() {
+        return atividadeSelecionada;
+    }
+
+    public void setAtividadeSelecionada(Atividade atividadeSelecionada) {
+        this.atividadeSelecionada = atividadeSelecionada;
+    }
+    
+    
+    
+    public List<Atividade> getListaAtividades() {
+        return new AtividadeService().listar();
+    }
+    
+    public String novaAtividade() {
+        atividadeSelecionada = new Atividade();
+        return ("cadastrarAtividade?faces-redirect=true");
+    }
+    
+    public String adicionarAtividade() {
+        try {
+            new AtividadeService().salvar(atividadeSelecionada);
+        } catch (BusinessException ex) {
+            // Erro
+        }
+        return (this.novaAtividade());
+        
+    }
+    
+    public String editarAtividade(Atividade at) {
+        atividadeSelecionada = at;
+        return ("editarAtividade?faces-redirect=true");
+    }
+    
+    public String atualizarAtividade() {
+        try {
+            new AtividadeService().atualizar(atividadeSelecionada);
+        } catch (BusinessException ex) {
+            //Mensagem de erro!
+        }
+        return ("editarAtividade?faces-redirect=true");
+    }
+    
+    public void removerAtividade(Atividade at) {
+        try {
+            new AtividadeService().excluir(atividadeSelecionada);
+        } catch (BusinessException ex) {
+            //Mensagem de erro!
+        }
     }
     
 }
